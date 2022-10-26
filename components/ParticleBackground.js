d1d8bee8326964dcd769772f7df44194af2dca01
viktor <viktor.saprykin@warwick.ac.uk>
2022-10-26 14:33:19 +0100

    added a full mobile version

--- components/ParticleBackground.js
+++ components/ParticleBackground.js
@@ -642,7 +642,14 @@ const ParticlesBack = () => {
 
 
     useEffect(() => {
+
     }, [isWeb3Enabled, data])
+
+    useEffect(() => {
+        setTimeout(() => {
+            window.scrollTo(0, 0);
+        }, 100);
+    }, [isSmallerThan721]);
 
 
 
@@ -662,20 +669,20 @@ const ParticlesBack = () => {
     return (
         <Box>
         <Particles id="tsparticles" init={particlesInit} loaded={particlesLoaded}
-        options={particlesConfig}/> <Center mt={"10vh"}> <Box width={"80%"}>
+        options={particlesConfig}/> <Center mt={ isSmallerThan721? "7" :"10vh"}> <Box width={"80%"}>
             <Title />
-            <Center> <HStack mt={"-17vh"} gap={"10vh"}> <SiBinance size={"10vh"} color={"#F3BA2F"}/> </HStack> </Center>
+            <Center> <HStack mt={isSmallerThan721? "2vh": "-17vh"} gap={"10vh"}> <Circle bg={"white"} size={"xm"}> <SiBinance size={"10vh"} color={"#F3BA2F"}/> </Circle> </HStack> </Center>
             <Center>
                 <Box >
-                    <Flex direction={ isSmallerThan721 ? "column" : "row"} ml={"60vh"} gap={"5vh"}>
-                        <Heading color={"white"}> A </Heading>
-                        <Heading color={"#F3BA2F"}> Free & Customizable </Heading>
+                    <Flex direction={ isSmallerThan721 ? "column" : "row"} ml={isSmallerThan721 ? 0 :"60vh"} gap={"5vh"} alignItems={"center"}>
+                        {isSmallerThan721 ? <> </> : <Heading color={"white"}> A </Heading>}
+                        <Heading mt={isSmallerThan721 ? "2vh" : 0} color={"#F3BA2F"}> Free & Customizable </Heading>
                         <Heading color={"white"}> BEP-20 </Heading>
                         <Heading color={"#F3BA2F"}> Token generator </Heading>
                     </Flex>
                 </Box>
             </Center>
-            <Center> <Box borderRadius={"5"}  mt={"10vh"} borderColor={"#F3BA2F"} maxW={"50%"}>
+            <Center> <Box borderRadius={"5"}  mt={"10vh"} borderColor={"#F3BA2F"} maxW={isSmallerThan721 ? "100%" : "50%"}>
             <Text fontSize={"xl"} color={"white"} align={"center"}> Welcome to BEPenator-2000, a decentralised web app designed to allow anyone the creation, deployment and automatic verification of custom BEP-20 token contracts on the Binance Smart Chain within
                 mere minutes.
                 Aside from the obligatory transaction cost on the Binance Smart Chain, completely free of charge requiring no setup, login, or coding. Was made to help people with no solidity coding experience but a need for a BEP-20 token to create their own quickly and for free and to help fellow web3 developers save their time coding boilerplate code. Enjoy :)     </Text>
@@ -694,7 +701,50 @@ const ParticlesBack = () => {
             </HStack>
              </Box> </Center>
             <Center> <Heading size={"xl"} mt={"20vh"} color={"#F3BA2F"}> ENTER DETAILS </Heading> </Center>
-            <Center mt={"10vh"}> <VStack gap={"10vh"}> <Flex>
+            <Center mt={"10vh"}> { isSmallerThan721 ?
+
+                <VStack gap={"10vh"}>
+
+                    <Flex width={"50vh"} direction={"column"}>
+                        <Heading size={"lg"} color={"white"}> Name: </Heading>
+                        <Input focusBorderColor={"teal.500"} width={"50vh"} color={"white"} borderWidth={"0.5vh"} borderColor={"#F3BA2F"} onChange={event => setName(event.target.value)}/>
+                        <Text alignSelf={"flex-end"} color={"white"}> What your token will be known as
+                        </Text>
+                    </Flex>
+                    <Flex direction={"column"} ml={"20vh"} width={"50vh"}>
+                        <Heading size={"lg"} color={"white"}> Symbol: </Heading>
+                        <Input focusBorderColor={"teal.500"} color={"white"} borderWidth={"0.5vh"} borderColor={"#F3BA2F"} onChange={event => setSymbol(event.target.value)}/>
+                        <Text alignSelf={"flex-end"} color={"white"}> Usually 3-5 capital letters
+                        </Text>
+                    </Flex>
+
+                    <Flex direction={"column"} width={"50vh"} mr={"20vh"}>
+                        <Heading size={"lg"} color={"white"}> Initial Supply: </Heading>
+                        <Input focusBorderColor={"teal.500"} width={"50vh"} color={"white"} borderWidth={"0.5vh"} borderColor={"#F3BA2F"} onChange={event => setInitialSupply(event.target.value)}/>
+                        <Text alignSelf={"flex-end"} color={"white"}>Sent to declared owner at token creation </Text>
+
+                    </Flex>
+                    <Flex direction={"column"} width={"50vh"} ml={"20vh"}>
+                        <Heading size={"lg"} color={"white"}> Owner: </Heading>
+                        <Input focusBorderColor={"teal.500"} width={"50vh"} color={"white"} borderWidth={"0.5vh"} borderColor={"#F3BA2F"} onChange={event => setOwner(event.target.value)}/>
+                        <Text alignSelf={"flex-end"} color={"white"}> Address that may have special privileges </Text> </Flex>
+
+
+
+                <Flex direction={"column"} width={"50vh"} mr={"20vh"}>
+                <Heading size={"lg"} color={"white"}> Decimals: </Heading>
+                <Input focusBorderColor={"teal.500"} width={"50vh"} color={"white"} borderWidth={"0.5vh"} borderColor={"#F3BA2F"} onChange={event => setDecimals(event.target.value)}/>
+                <Text alignSelf={"flex-end"} color={"white"}> Recommended/Usually - 18 </Text>
+                </Flex>
+                <Flex direction={"column"} width={"50vh"} ml={"20vh"}>
+                <Heading size={"lg"} color={"white"}> Cap: </Heading>
+                <Input isDisabled={ supply !== "capped"} focusBorderColor={"teal.500"} width={"50vh"} color={"white"} borderWidth={"0.5vh"} borderColor={"#F3BA2F"} onChange={event => setCap(event.target.value)}/>
+                <Text alignSelf={"flex-end"} color={"white"}> Only works if Capped Supply is selected </Text>
+                </Flex> </VStack>
+
+
+
+                : <VStack gap={"10vh"}> <Flex>
 
                 <Flex width={"50vh"} direction={"column"} mr={"20vh"}>
                     <Heading size={"lg"} color={"white"}> Name: </Heading>
@@ -744,16 +794,15 @@ const ParticlesBack = () => {
                     </Flex>
 
 
-                </Flex>
+                </Flex> </VStack> }
 
-            </VStack>
 
             </Center>
             <Center> <Heading size={"xl"} mt={"20vh"} color={"#F3BA2F"}> CHOOSE SUPPLY </Heading> </Center>
             <RadioGroup onChange={setSupply} value={supply}>
-                <Center> <Flex mt={"10vh"} direction='row'>
+                <Center> <Flex mt={"10vh"} direction={isSmallerThan721 ? "column" : "row"} rowGap={"5vh"}>
 
-                    <Box maxW={"31%"}> <ScaleFade whileHover={{ scale: 1.2 }}
+                    <Box maxW={isSmallerThan721 ? "100%" :"31%"}> <ScaleFade whileHover={{ scale: 1.2 }}
                                initialScale={1}
                                in={ 1===1} > <VStack spacing={"4vh"}>  <Heading color={"white"}> Fixed </Heading> <Radio colorScheme='yellow' value='fixed'> </Radio>
                        <Text align={"center"} color={"white"} > Impossible to generate new tokens. Max supply will be the declared initial supply.</Text>
@@ -769,7 +818,7 @@ const ParticlesBack = () => {
 
 
 
-                    <Box maxW={"30%"}> <ScaleFade whileHover={{ scale: 1.2 }}
+                    <Box maxW={isSmallerThan721 ? "100%" :"31%"}> <ScaleFade whileHover={{ scale: 1.2 }}
                                                   initialScale={1}
                                                   in={ 1===1} ><VStack  spacing={"4vh"}>  <Heading color={"white"}> Capped </Heading> <Radio colorScheme='yellow' value='capped'> </Radio>
                         <Text align={"center"} color={"white"}> Impossible to generate more tokens than the defined token cap.</Text>
@@ -780,9 +829,9 @@ const ParticlesBack = () => {
 
             <Center> <Heading size={"xl"} mt={"20vh"} color={"#F3BA2F"}> CHOOSE OWNERSHIP </Heading> </Center>
             <RadioGroup onChange={setOwnership} value={ownership}>
-                <Center> <Flex mt={"10vh"} direction='row'>
+                <Center> <Flex mt={"10vh"} direction={isSmallerThan721 ? "column" : "row"} rowGap={"5vh"}>
 
-                    <Box maxW={"31%"}> <ScaleFade whileHover={{ scale: 1.2 }}
+                    <Box maxW={isSmallerThan721 ? "100%" :"31%"}> <ScaleFade whileHover={{ scale: 1.2 }}
                                                   initialScale={1}
                                                   in={ 1===1} ><VStack spacing={"4vh"}>  <Heading color={"white"}> No Owner </Heading> {supply !== "fixed" ? <Text color={"#624b14"}> Select fixed supply first</Text> : <Radio  colorScheme='yellow' value='none'> </Radio> }
                         <Text align={"center"} color={"white"} > No account has special privileges. Tokens cannot be minted or paused. Initial supply is still sent to declared owner account.</Text>
@@ -798,7 +847,7 @@ const ParticlesBack = () => {
 
 
 
-                            <Box maxW={"31%"}> <ScaleFade whileHover={{ scale: 1.2 }}
+                            <Box maxW={isSmallerThan721 ? "100%" :"31%"}> <ScaleFade whileHover={{ scale: 1.2 }}
                                                           initialScale={1}
                                                           in={ 1===1} ><VStack spacing={"4vh"}>  <Heading color={"white"}> Role-Based </Heading> <Radio colorScheme='yellow' value='role'> </Radio>
                         <Text align={"center"} color={"white"}> Declared owner becomes the admin and can give other accounts roles of either itself, minter or pauser if the contract allows those actions.</Text>
@@ -806,11 +855,12 @@ const ParticlesBack = () => {
 
                 </Flex> </Center>
             </RadioGroup>
+            {isSmallerThan721 ? <Center> <VStack> <Heading size={"xl"} mt={"20vh"} color={"#F3BA2F"}> PICK AVAILABLE </Heading> <Heading size={"xl"} mt={"20vh"} color={"#F3BA2F"}> ACTIONS </Heading> </VStack> </Center>
+                :
+            <Center> <Heading size={"xl"} mt={"20vh"} color={"#F3BA2F"}> PICK AVAILABLE ACTIONS </Heading> </Center>}
 
-            <Center> <Heading size={"xl"} mt={"20vh"} color={"#F3BA2F"}> PICK AVAILABLE ACTIONS </Heading> </Center>
 
-
-            <Center> <HStack mt={"20vh"} spacing={"10vh"}>
+            <Center> <Flex direction={isSmallerThan721 ? "column" : "row"} mt={"20vh"} columnGap={"10vh"} rowGap={"10vh"}>
 
                 <ScaleFade whileHover={{ scale: 1.4 }}
                            initialScale={1}
@@ -846,20 +896,20 @@ const ParticlesBack = () => {
                 </VStack> </ScaleFade>
 
 
-            </HStack> </Center>
+            </Flex> </Center>
 
             <Center> <Heading size={"xl"} mt={"20vh"} color={"#F3BA2F"}> AND FINALLY... </Heading> </Center>
 
             <Center mt={"5vh"}> <Circle onClick={DeployToken} borderWidth={"1vh"} borderColor={"white"} as={Button} bg={"#F3BA2F"} size={"3xs"} colorScheme={"yellow"} color={"white"}> <VStack> <Heading> CREATE  </Heading> <Heading> TOKEN  </Heading></VStack> </Circle> </Center>
-            <Center> <VStack> <Heading size={"md"} mt={"10vh"} color={"white"}> Once you get the notification about the success of your token deployment</Heading> <Heading size={"md"} mt={"10vh"} color={"white"}> usually within a few minutes </Heading> </VStack> </Center>
+            <Center> <VStack mt={isSmallerThan721 ? "5vh" : 0}> {isSmallerThan721 ? <Text align={"center"} color={"white"}> Within a few minutes you will get a notification about the success of your token deployment </Text> : <Heading size={"md"} mt={"10vh"} color={"white"}> Within a few minutes you will get a notification about the success of your token deployment</Heading>}<Heading size={"md"} mt={"10vh"} color={"white"}> After that  </Heading>  </VStack> </Center>
 
             <Center> <Heading size={"md"} mt={"10vh"} color={"white"}> Or! </Heading> </Center>
-            <Center> <Heading size={"md"} mt={"3vh"} color={"#F3BA2F"}> If you ever miss/forget/lose your token address </Heading> </Center>
+            <Center> { isSmallerThan721 ? <Text fontSize={"xl"} align={"center"} mt={"3vh"} color={"#F3BA2F"}> If you ever miss/forget/lose your token address </Text> : <Heading size={"md"} mt={"3vh"} color={"#F3BA2F"}> If you ever miss/forget/lose your token address </Heading> }</Center>
             <Center> <Heading size={"md"} mt={"3vh"} color={"white"}> You can use </Heading> </Center>
 
             <Box>
-             <Box mt={"15vh"}> <Center padding={"1vh"} mt={"1vh"} height={"100%"}> <VStack> <Heading color={"white"} size={"3xl"}> BEPENATOR LOOKUP </Heading>
-             <HStack> <Heading size={"lg"} color={"white"}> Owner Address </Heading> <BsArrowRight color={"#F3BA2F"} size={"8vh"}/> <Heading size={"lg"} color={"white"}> Tokens Created  </Heading> </HStack> </VStack>
+                <Box mt={"15vh"}> <Center padding={"1vh"} mt={"1vh"} height={"100%"}> <VStack> {isSmallerThan721 ? <VStack> <Heading color={"white"} size={"3xl"}> BEPENATOR  </Heading>  <Heading color={"white"} size={"3xl"}> LOOKUP </Heading></VStack> : <Heading color={"white"} size={"3xl"}> BEPENATOR LOOKUP </Heading>}
+             <HStack> <Center> <Heading size={"lg"} color={"white"}> {isSmallerThan721? "Owner" :"Owner Address"} </Heading> <BsArrowRight color={"#F3BA2F"} size={"10vh"}/> <Heading size={"lg"} color={"white"}> Tokens Created  </Heading> </Center> </HStack> </VStack>
              </Center> </Box>
                     <VStack width={"100%"} height={"10vh"} marginTop={"5vh"}>
 
--- components/Title.js
+++ components/Title.js
@@ -1,15 +1,28 @@
-import {Box, Center, Flex, Heading, HStack, Text} from "@chakra-ui/react";
+import {Box, Center, Flex, Heading, HStack, Text, useMediaQuery, VStack} from "@chakra-ui/react";
 import "@fontsource/silkscreen/400.css"
 
 const Title = () => {
 
-    return (
+    const [isSmallerThan721] = useMediaQuery("(max-width: 721px)")
+
+
+    return ( isSmallerThan721 ? <Center> <VStack>
+
+            <Flex>
+                <Heading size={"4xl"} color={"#F3BA2F"} letterSpacing={"1vh"}> BEP </Heading>
+                <Heading size={"4xl"} color={"white"} letterSpacing={"1vh"}> ERATOR </Heading>
+            </Flex>
+
+                <Flex> <Heading size={"4xl"} color={"#F3BA2F"} letterSpacing={"1vh"}> 20 </Heading>
+                <Heading size={"4xl"} color={"white"} letterSpacing={"1vh"}> 00 </Heading> </Flex>
+                </VStack>
+            </Center> :
             <Flex direction={"column"} alignItems={"center"} rowGap={"20vh"}>
                 <Flex mr={"35vh"}>
-                    <Heading size={"4xl"} color={"#F3BA2F"} letterSpacing={"2vh"}> BEP </Heading>
-                    <Heading size={"4xl"} color={"white"} letterSpacing={"2vh"}> ERATOR- </Heading>
-                    <Heading size={"4xl"} color={"#F3BA2F"} letterSpacing={"2vh"}> 20 </Heading>
-                    <Heading size={"4xl"} color={"white"} letterSpacing={"2vh"}> 00 </Heading>
+                    <Heading size={"4xl"} color={"#F3BA2F"} letterSpacing={"1vh"}> BEP </Heading>
+                    <Heading size={"4xl"} color={"white"} letterSpacing={"1vh"}> ERATOR- </Heading>
+                    <Heading size={"4xl"} color={"#F3BA2F"} letterSpacing={"1vh"}> 20 </Heading>
+                    <Heading size={"4xl"} color={"white"} letterSpacing={"1vh"}> 00 </Heading>
                 </Flex>
                 <Center>  </Center>
 
